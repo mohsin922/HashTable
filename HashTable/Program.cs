ce5903249efd779cd8a8582e6bb5d88bb108dbd1
peermohsin22 <peermohsin22@gmail.com>
2022-01-01 19:14:27 +0530

    Frequecy Of Words

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -7,6 +7,19 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hash Table!");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            Console.WriteLine("Frequency of \'to\'  : " + hash.GetFrequencyOfWords("to"));
+            Console.WriteLine("Frequency of \'be\' : " + hash.GetFrequencyOfWords("be"));
+            Console.WriteLine("Frequency of \'or\' : " + hash.GetFrequencyOfWords("or"));
+            Console.WriteLine("Frequency of \'not\' : " + hash.GetFrequencyOfWords("not"));
+
         }
     }
 }
